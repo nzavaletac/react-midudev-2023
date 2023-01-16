4e79d8b434858b8f1796d2fbbccfb9122ba53521
Noe Zavaleta C <nzavaletac@gmail.com>
2023-01-16 17:40:59 -0500

    Add first class

--- projects/hola-mundo/src/App.jsx
+++ projects/hola-mundo/src/App.jsx
@@ -1,33 +1,17 @@
 import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 export const App = () => {
-  const formatUserName = (userName) => `@${userName}`;
+  const midudev = {
+    userName: "midudev",
+    name: "Miguel Angel Durand",
+  };
+
   return (
     <section className="App">
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        userName="nzavaletac"
-        name="Noe Zavaleta Cardeña"
-        isFollowing={true}
-      />
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        userName="midudev"
-        name="Miguel Angel Durand"
-        isFollowing={false}
-      />
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        userName="pheralb"
-        name="Pablo Hernadez"
-        isFollowing={true}
-      />
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        userName="lionelmessi"
-        name="Leonel Messi"
-        isFollowing={false}
-      />
+      <TwitterFollowCard userName="nzavaletac" name="Noe Zavaleta Cardeña" />
+      <TwitterFollowCard {...midudev} />
+      <TwitterFollowCard userName="pheralb" name="Pablo Hernadez" />
+      <TwitterFollowCard userName="lionelmessi" name="Leonel Messi" />
     </section>
   );
 };
--- projects/hola-mundo/src/TwitterFollowCard.jsx
+++ projects/hola-mundo/src/TwitterFollowCard.jsx
@@ -1,10 +1,18 @@
-export const TwitterFollowCard = ({
-  formatUserName,
-  userName,
-  name,
-  isFollowing,
-}) => {
+import { useState } from "react";
+
+export const TwitterFollowCard = ({ userName, name }) => {
   const imageSrc = `https://unavatar.io/${userName}`;
+
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = isFollowing
+    ? "tw-followCard-button is-following"
+    : "tw-followCard-button";
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
 
   return (
     <article className="tw-followCard">
@@ -17,14 +25,15 @@ export const TwitterFollowCard = ({
         />
         <div className="tw-followCard-info">
           <strong>{name}</strong>
-          <span className="tw-followCard-infoUserName">
-            {formatUserName(userName)}
-          </span>
+          <span className="tw-followCard-infoUserName">@{userName}</span>
         </div>
       </header>
 
       <aside>
-        <button className="tw-followCard-button">Seguir</button>
+        <button className={buttonClassName} onClick={handleClick}>
+          <span className="tw-followCard-text"> {text}</span>
+          <span className="tw-followCard-stopFollow">Dejar de Seguir</span>
+        </button>
       </aside>
     </article>
   );
